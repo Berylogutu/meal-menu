c2d9f9129b5dc7683a4d8262b34f4dc3bc32e78b
Berylogutu <berylogutu43@gmail.com>
2023-07-08 11:51:59 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import { SearchBar } from "./SearchBar";
 import { Meal } from './Meal'
 import {style} from './style.css'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
+
+  const [meal, setMeal] = useState({});
+  const [showRecipe, setShowRecipe] = useState(false)
+
+  useEffect(() => {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then(res => res.json())
+      .then(data => setMeal(data.meals[0]))
+      .catch(err => console.log(err));
+  }, []);
+
+
+
+  function showInfo(e) {
+    const { src, alt } = e.target;
+
+   
+      setMeal(prevMeal => ({
+        ...prevMeal,
+        strInstructions: prevMeal.idMeal === meal.idMeal ? meal.strInstructions : prevMeal.strInstructions
+      }));
+    setShowRecipe(true)
+    
+  }
 
   return (
     
     <>
     <h3 className="text-red" >Hello, welcome to FoodZone</h3>
     <SearchBar />
-    <Meal />
+    <Meal 
+    meal={meal}
+    showRecipe={showRecipe}
+    showInfo={showInfo}
+    
+    />
     </>
   )
 }
--- src/Meal.js
+++ src/Meal.js
@@ -1,28 +1,7 @@
-import { useState, useEffect } from 'react';
 
-export function Meal() {
-  const [meal, setMeal] = useState({});
-  const [showRecipe, setShowRecipe] = useState(false)
 
-  useEffect(() => {
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-      .then(res => res.json())
-      .then(data => setMeal(data.meals[0]))
-      .catch(err => console.log(err));
-  }, []);
-
-  function showInfo(e) {
-    const { src, alt } = e.target;
-
-   
-      setMeal(prevMeal => ({
-        ...prevMeal,
-        strInstructions: prevMeal.idMeal === meal.idMeal ? meal.strInstructions : prevMeal.strInstructions
-      }));
-    setShowRecipe(true)
-    
-  }
-
+export function Meal({meal, showRecipe, showInfo}) {
+ 
   return (
     <>
       <div className='meal container'>
